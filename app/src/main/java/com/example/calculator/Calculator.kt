d460ff0033d8e9fbfd7de471b4bef1aeb2e92c03
Sameer <sameer.ali097643@gmail.com>
2023-12-19 22:38:22 +0530

    Calculator Completed

--- app/src/main/java/com/example/calculator/Calculator.kt
+++ app/src/main/java/com/example/calculator/Calculator.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
@@ -14,7 +13,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
@@ -26,11 +24,11 @@ import com.example.calculator.ui.theme.Orange
 @Composable
 fun Calculator(
     state: CalculatorState,
-    buttonSpacing: Dp = 8.dp,
     modifier: Modifier = Modifier,
+    buttonSpacing: Dp = 8.dp,
     onAction: (CalculatorAction) -> Unit //onAction is the Parameter which receives a callback function. Unit is the return type.
 ) {
-    Box() {
+    Box(modifier = Modifier) {
 
         Column(
             modifier = Modifier
@@ -39,7 +37,7 @@ fun Calculator(
             verticalArrangement = Arrangement.spacedBy(buttonSpacing)
         ) {
             Text(
-                text = state.number1 + (state.operation ?: "") + state.number2,
+                text = state.number1 + (state.operation?.symbol ?: "") + state.number2,
                 textAlign = TextAlign.End,
                 modifier = Modifier
                     .fillMaxWidth()
